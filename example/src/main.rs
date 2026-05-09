6ce02aa75185ab50eaa7100516c20eb24d28098b
so-groenen <solofo.groenendijk@gmail.com>
2026-05-09 18:12:46 +0200

    replaced timer by Ticker; used integer clock divider for clk

--- example/src/main.rs
+++ example/src/main.rs
@@ -4,12 +4,12 @@
 
 use embassy_executor::Spawner;
 use embassy_rp::spi::{self, Spi};
-use embassy_rp::{bind_interrupts};
+use embassy_rp::{bind_interrupts, clocks};
 use embassy_rp::peripherals::{DMA_CH0}; 
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp_st7789::st7789::{ST7789Display, Rotation, NoPin};
 use embassy_rp_st7789::colors;
-use embassy_time::Timer;
+use embassy_time::{Duration, Ticker};
 use defmt::*;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -21,6 +21,12 @@ bind_interrupts!(struct Irqs {
 const NYAN_WIDTH: u16  = 220;
 const NYAN_HEIGTH: u16 = 220;
 
+const DISPLAY_WIDTH: u16  = 240;
+const DISPLAY_HEIGTH: u16 = 240;
+
+const X: u16 = (DISPLAY_WIDTH - NYAN_WIDTH)/2;
+const Y: u16 = (DISPLAY_HEIGTH - NYAN_HEIGTH)/2;
+
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) 
@@ -28,37 +34,49 @@ async fn main(_spawner: Spawner)
     let p = embassy_rp::init(Default::default());
 
     let mut config   = spi::Config::default();
-    config.frequency = 50_000_000; 
+    config.frequency = clocks::clk_sys_freq()/4; // 155/4 = 37.5Mhz; 
+    config.polarity  = spi::Polarity::IdleHigh;
+    config.phase     = spi::Phase::CaptureOnSecondTransition;
+    
+    // SPI Display wire color coding suggestion: 
+    // vcc: red
+    // gnd: black
+    // mosi: green
+    // sclk: orange
+    // dc: violent
+    // cs: yellow
+    // bl: blue
 
-    let spi = Spi::new_txonly(p.SPI0, p.PIN_6, p.PIN_7, p.DMA_CH0, Irqs, config);
+    let spi = Spi::new_txonly(p.SPI0, p.PIN_2, p.PIN_3, p.DMA_CH0, Irqs, config);
 
     let cat_gif = include_bytes!("../gifs_to_bin/nyan_220x220_frames=17.bin"); 
     let reset_pin = NoPin::new();
-    let dc_pin = Output::new(p.PIN_9, Level::High);
-    let cs_pin = Output::new(p.PIN_5, Level::High);
-    let bl_pin = Output::new(p.PIN_4, Level::High);
+    let dc_pin = Output::new(p.PIN_4, Level::High); 
+    let cs_pin = Output::new(p.PIN_5, Level::High); 
+    let bl_pin = Output::new(p.PIN_6, Level::High); 
 
-    let mut display = ST7789Display::new(reset_pin, dc_pin, cs_pin, bl_pin, spi, 240, 240, Rotation::Landscape).await
+    let mut display = ST7789Display::new(reset_pin, dc_pin, cs_pin, bl_pin, spi, DISPLAY_WIDTH, DISPLAY_HEIGTH, Rotation::Landscape).await
                         .expect("Critical: Could not init display!");
- 
-    display.fill(colors::BLACK).await.unwrap();
+    
+    display.fill(colors::BLACK).await.expect("Could not fill display");
  
     let frames          = 17;
     let bytes_per_frame = cat_gif.len() / frames;
     let mut n           = 0;
-    
+    let mut ticker      = Ticker::every(Duration::from_millis(33));
+
     loop
     {
         let start = n*bytes_per_frame;
         let stop  = (n+1)*bytes_per_frame;
-        if let Err(e) = display.draw_color_buf_raw(&cat_gif[start..stop], 10, 10, NYAN_WIDTH, NYAN_HEIGTH).await
+        if let Err(e) = display.draw_color_buf_raw(&cat_gif[start..stop], X, Y, NYAN_WIDTH, NYAN_HEIGTH).await
         {
             error!("Could not send data to display: {:?}", e);
             return;
         }
 
         n = (n + 1)%frames;
-        Timer::after_millis(50).await;  
+        ticker.next().await;
     }
 }
 
